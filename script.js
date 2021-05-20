f4eda1ddf0caab2a52a57f2c0a78f688976a1df5
Konstantin Radzevich <rodzevich.k@gmail.com>
2021-05-20 19:36:58 +0300

    add rub start js

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-function convert(param1){
-    var factor;
-    f = document.getElementById('from').value
+function convert(param1) {
+    var factor1;
+    var factor2;
+    e = document.getElementById('from').value
     s = document.getElementById('to').value
-    t = document.getElementById('to2').value
+    r = document.getElementById('to2').value
 
-    if(f == t){
+    if (e == s && e == r) {
         factor = 1
-    }
-    else if(f == 'EUR' && s == 'USD'){
+        factor2 = 1
+    } else if (e == 'EUR' && s == 'USD') {
         factor = 1.06;
-    }
-    // else if(f == 'USD' && s == 'EUR'){
-    //     factor = 1/1.06;
-    // }
-    else if(f == 'RUB' && t == 'EUR'){
-        factor = 0.0106;
+        factor2 = 89.87;
+    } else if (s == 'USD' && e == 'EUR') {
+        factor = 1 / 1.06;
+    } else if (r == 'RUB' && e == 'EUR') {
+        factor2 = 0.0106;
     }
 
 
-    if(param1 == "C"){
+    if (param1 == "C") {
         document.getElementById('secondinput').value = document.getElementById('firstinput').value * factor
+        document.getElementById('therthdinput').value = document.getElementById('firstinput').value * factor2
     }
-    // if(param1 == "F"){
-    //     document.getElementById('firstinput').value = document.getElementById('secondinput').value * factor
-    // }
-    if(param1 == "D"){
-        document.getElementById('firstinput').value = document.getElementById('therthdinput').value * factor
+    if (param1 == "F") {
+        document.getElementById('firstinput').value = document.getElementById('secondinput').value * factor
+    }
+    if (param1 == "D") {
+        document.getElementById('therthdinput').value = document.getElementById('therthdinput').value * factor2
     }
 
 }
