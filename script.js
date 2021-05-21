6b6112adf67cbc946f771cf351d9ed02c79a6f66
Kanstantsin <Radzevich.k@gmail.com>
2021-05-21 11:10:30 +0300

    del trash functions

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 function convert(param1) {
+    var usdId = 145;
+    var eurId = 293;
+    var plnId = 355; //10pln
+    var rubId = 298; //100rub
+    var kztId = 302; //1000
+    
+    
+    
     var factor1;
     var factor2;
-    e = document.getElementById('from').value
-    s = document.getElementById('to').value
-    r = document.getElementById('to2').value
+    e = document.getElementById('EUR').value
+    s = document.getElementById('USD').value
+    r = document.getElementById('RUB').value
+    p = document.getElementById('PLN').value
+    k = document.getElementById('KZT').value
 
     if (e == s && e == r) {
-        factor = 1
+        factor1 = 1
         factor2 = 1
     } else if (e == 'EUR' && s == 'USD') {
-        factor = 1.06;
+        factor1 = 1.06;
         factor2 = 89.87;
     } else if (s == 'USD' && e == 'EUR') {
-        factor = 1 / 1.06;
+        factor1 = 1 / 1.06;
     } else if (r == 'RUB' && e == 'EUR') {
         factor2 = 0.0106;
     }
@@ -33,58 +43,98 @@ function convert(param1) {
 var uri = 'https://www.nbrb.by/API/';
 $(function() {
     $('#btn').click(function() {
-       rates(0);
+       rates2();
     });
 
-    function currencies() {
-        $.getJSON(uri + 'ExRates/Currencies')
-            .done(function(data) {
-                $.each(data, function(key, item) {
-                    $('<li>', {
-                        text: JSON.stringify(item)
-                    }).appendTo($('#res'));
-                });
-                $('#btn').removeAttr("disabled");
-            }).error(function(err) {
-                $('#btn').removeAttr("disabled");
-                alert('ошибка');
-            });
-    };
+    // function currencies() {
+    //     $.getJSON(uri + 'ExRates/Currencies')
+    //         .done(function(data) {
+    //             $.each(data, function(key, item) {
+    //                 $('<li>', {
+    //                     text: JSON.stringify(item)
+    //                 }).appendTo($('#res'));
+    //             });
+    //             $('#btn').removeAttr("disabled");
+    //         }).error(function(err) {
+    //             $('#btn').removeAttr("disabled");
+    //             alert('ошибка');
+    //         });
+    // };
 
-    function currency() {
-        $.getJSON(uri + 'ExRates/Currencies/' + $('#iCur').val())
-            .done(function(data) {
-                $('<li>', {
-                    text: JSON.stringify(data)
-                }).appendTo($('#res'));
-                $('#btn').removeAttr("disabled");
-            }).error(function(err) {
-                $('#btn').removeAttr("disabled");
-                alert('ошибка');
-            });
-    };
+    // function currency() {
+    //     $.getJSON(uri + 'ExRates/Currencies/' + $('#iCur').val())
+    //         .done(function(data) {
+    //             $('<li>', {
+    //                 text: JSON.stringify(data)
+    //             }).appendTo($('#res'));
+    //             $('#btn').removeAttr("disabled");
+    //         }).error(function(err) {
+    //             $('#btn').removeAttr("disabled");
+    //             alert('ошибка');
+    //         });
+    // };
 
-    function rates(p) {
-        $.getJSON(uri + 'ExRates/Rates', {
-                'onDate': new Date($('#iDate').val()).toUTCString(),
-                'Periodicity': p
-            })
-            .done(function(data) {
-                $.each(data, function(key, item) {
-                    $('<li>', {
-                        text: JSON.stringify(item)
-                    }).appendTo($('#res'));
-                });
-                $('#btn').removeAttr("disabled");
-            }).error(function(err) {
-                $('#btn').removeAttr("disabled");
-                alert('ошибка');
-            });
-    };
+    // function rates(p) {
+    //     $.getJSON(uri + 'ExRates/Rates', {
+    //             'onDate': new Date($('#iDate').val()).toUTCString(),
+    //             'Periodicity': p
+    //         })
+    //         .done(function(data) {
+    //             $.each(data, function(key, item) {
+    //                 $('<li>', {
+    //                     text: JSON.stringify(item)
+    //                 }).appendTo($('#res'));
+    //             });
 
-   
+                
+    //             $('#btn').removeAttr("disabled");
+    //         }).error(function(err) {
+    //             $('#btn').removeAttr("disabled");
+    //             alert('ошибка');
+    //         });
+    // };
+    
+    
 });
+function rates2() {
+    var curRates = [];
+    $.getJSON(uri + 'ExRates/Rates', {
+            'onDate': new Date($('#iDate').val()).toUTCString(),
+            'Periodicity': '0'
+        })
+        .done(function(data) {
+           $.each(data, function(key, item) {
+             if(item["Cur_ID"] == 145){
+                 
+                 curRates.push([item["Cur_OfficialRate"]])
+             }
+             if(item["Cur_ID"] == 292){
+               
+                curRates.push([item["Cur_OfficialRate"]])
+            }
+            if(item["Cur_ID"] == 293){
+               
+                curRates.push([item["Cur_OfficialRate"]])
+            }
+            if(item["Cur_ID"] == 298){
+               
+                curRates.push([item["Cur_OfficialRate"]])
+            }
+            if(item["Cur_ID"] == 301){
+               
+                curRates.push([item["Cur_OfficialRate"]])
+            }   
 
-function clearData(){
-    $("#ul").remove(); 
-    };
+           
+     
+            });
+        })
+        console.log(curRates)
+        convert(curRates);
+        return curRates;
+       
+    }
+
+// function clearData(){
+//     $("#ul").remove(); 
+//     };
