501245873f7df07d72d6afaf005000509025b238
Konstantin Radzevich <rodzevich.k@gmail.com>
2021-05-23 08:55:24 +0300

    All is done

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
 var uri = 'https://www.nbrb.by/API/';
 $(function() {
     $('#btn').click(function() {
-       rates2();
+        rates2();
     });
 
 });
+
 function rates2(e) {
     var curRates = [];
     $.getJSON(uri + 'ExRates/Rates', {
@@ -14,84 +14,95 @@ function rates2(e) {
         })
         .done(function(data) {
             $.each(data, function(key, item) {
-            
-             if(item["Cur_ID"] == 145){
-                 
-                 curRates.push(item["Cur_OfficialRate"])
-             }
-             if(item["Cur_ID"] == 292){
-               
-                curRates.push(item["Cur_OfficialRate"])
-            }
-            if(item["Cur_ID"] == 293){
-               
-                curRates.push(item["Cur_OfficialRate"])
-            }
-            if(item["Cur_ID"] == 298){
-               
-                curRates.push(item["Cur_OfficialRate"])
-            }
-            if(item["Cur_ID"] == 301){
-               
-                curRates.push(item["Cur_OfficialRate"])
-            }   
-    
+
+                if (item["Cur_ID"] == 145) {
+
+                    curRates.push(item["Cur_OfficialRate"])
+                }
+                if (item["Cur_ID"] == 292) {
+
+                    curRates.push(item["Cur_OfficialRate"])
+                }
+                if (item["Cur_ID"] == 293) {
+
+                    curRates.push(item["Cur_OfficialRate"])
+                }
+                if (item["Cur_ID"] == 298) {
+
+                    curRates.push(item["Cur_OfficialRate"])
+                }
+                if (item["Cur_ID"] == 301) {
+
+                    curRates.push(item["Cur_OfficialRate"])
+                }
+
             });
-            
-       
-        crossCur(curRates, e);
-    });
 
-    
+
+            crossCur(curRates, e);
+        });
+
+
 }
-    function crossCur(curRates, e) {
-    
-    
-        var usdId = parseFloat(curRates[0], 10); //USD
-        var eurId = parseFloat(curRates[1], 10); //EUR
-        var plnId = parseFloat(curRates[2], 10); //10pln
-        var rubId = parseFloat(curRates[3], 10); //100rub
-        var kztId = parseFloat(curRates[4], 10); //1000
-        console.log(usdId / eurId)
-        
-        
-        
-        if (e == "E") {
-           document.getElementById('USD').value = document.getElementById('EUR').value *eurId/usdId;
-           document.getElementById('RUB').value = document.getElementById('EUR').value *eurId*100/rubId;
-           document.getElementById('PLN').value = document.getElementById('EUR').value *eurId*10/plnId;
-           document.getElementById('KZT').value = document.getElementById('EUR').value *eurId*1000/kztId;
-        }
-        if (e == "U") {
-            
-           document.getElementById('EUR').value = document.getElementById('USD').value *usdId/eurId;
-           document.getElementById('RUB').value = document.getElementById('USD').value *usdId/eurId *eurId*100/rubId;
-           document.getElementById('PLN').value = document.getElementById('USD').value *usdId/eurId *eurId*10/plnId;
-           document.getElementById('KZT').value = document.getElementById('USD').value *usdId/eurId *eurId*1000/kztId;
-        }
-        if (e == "R") {
-            
-            document.getElementById('EUR').value = document.getElementById('RUB').value *eurId*100/rubId/eurId;
-            document.getElementById('USD').value = document.getElementById('RUB').value *eurId*100/rubId /eurId *eurId/usdId;
-            document.getElementById('PLN').value = document.getElementById('RUB').value *eurId*100/rubId /eurId *eurId*10/plnId;
-            document.getElementById('KZT').value = document.getElementById('RUB').value *eurId*100/rubId /eurId *eurId*1000/kztId;
-         }
-         if (e == "P") {
-            
-            document.getElementById('EUR').value = document.getElementById('PLN').value *eurId*10/plnId/eurId;
-            document.getElementById('USD').value = document.getElementById('PLN').value *eurId*10/plnId/eurId*eurId/usdId;
-            document.getElementById('RUB').value = document.getElementById('PLN').value *eurId*10/plnId/eurId*eurId*100/rubId;
-            document.getElementById('KZT').value = document.getElementById('PLN').value *eurId*10/plnId/eurId*eurId*1000/kztId;
-         }
-         if (e == "K") {
-            
-            document.getElementById('EUR').value = document.getElementById('KZT').value *eurId*1000/kztId/eurId;
-            document.getElementById('USD').value = document.getElementById('KZT').value *eurId*1000/kztId/usdId;
-            document.getElementById('PLN').value = document.getElementById('KZT').value *eurId*1000/kztId*10/plnId;
-            document.getElementById('RUB').value = document.getElementById('KZT').value *eurId*1000/kztId*100/rubId;
-         }
-    };
 
-  
-  
+function crossCur(curRates, e) {
 
+
+    var usdId = parseFloat(curRates[0], 10); //USD
+    var eurId = parseFloat(curRates[1], 10); //EUR
+    var plnId = parseFloat(curRates[2], 10); //10pln
+    var rubId = parseFloat(curRates[3], 10); //100rub
+    var kztId = parseFloat(curRates[4], 10); //1000
+    console.log(usdId / eurId)
+
+
+
+    if (e == "E") {
+        document.getElementById('USD').value = document.getElementById('EUR').value * eurId / usdId;
+        document.getElementById('RUB').value = document.getElementById('EUR').value * eurId * 100 / rubId;
+        document.getElementById('PLN').value = document.getElementById('EUR').value * eurId * 10 / plnId;
+        document.getElementById('KZT').value = document.getElementById('EUR').value * eurId * 1000 / kztId;
+        document.getElementById('BYN').value = document.getElementById('EUR').value * eurId;
+    }
+    if (e == "U") {
+
+        document.getElementById('EUR').value = document.getElementById('USD').value * usdId / eurId;
+        document.getElementById('RUB').value = document.getElementById('USD').value * usdId / eurId * eurId * 100 / rubId;
+        document.getElementById('PLN').value = document.getElementById('USD').value * usdId / eurId * eurId * 10 / plnId;
+        document.getElementById('KZT').value = document.getElementById('USD').value * usdId / eurId * eurId * 1000 / kztId;
+        document.getElementById('BYN').value = document.getElementById('USD').value * usdId;
+    }
+    if (e == "R") {
+
+        document.getElementById('EUR').value = document.getElementById('RUB').value * rubId / 100 / eurId;
+        document.getElementById('USD').value = document.getElementById('RUB').value * rubId / 100 / usdId;
+        document.getElementById('PLN').value = document.getElementById('RUB').value * rubId / 100 / plnId * 10;
+        document.getElementById('KZT').value = document.getElementById('RUB').value * rubId / 100 / kztId * 1000;
+        document.getElementById('BYN').value = document.getElementById('RUB').value * rubId / 100;
+
+    }
+    if (e == "P") {
+
+        document.getElementById('EUR').value = document.getElementById('PLN').value * plnId / 10 / eurId;
+        document.getElementById('USD').value = document.getElementById('PLN').value * plnId / 10 / usdId;
+        document.getElementById('RUB').value = document.getElementById('PLN').value * plnId / 10 / rubId * 100;
+        document.getElementById('KZT').value = document.getElementById('PLN').value * plnId / 10 / kztId * 1000;
+        document.getElementById('BYN').value = document.getElementById('PLN').value * plnId / 10;
+    }
+    if (e == "K") {
+
+        document.getElementById('EUR').value = document.getElementById('KZT').value * kztId / 1000 / eurId;
+        document.getElementById('USD').value = document.getElementById('KZT').value * kztId / 1000 / usdId;
+        document.getElementById('PLN').value = document.getElementById('KZT').value * kztId / 1000 / plnId * 10;
+        document.getElementById('RUB').value = document.getElementById('KZT').value * kztId / 1000 / rubId * 100;
+        document.getElementById('BYN').value = document.getElementById('KZT').value * kztId / 1000;
+    }
+    if (e == "B") {
+
+        document.getElementById('EUR').value = document.getElementById('BYN').value / eurId;
+        document.getElementById('USD').value = document.getElementById('BYN').value / usdId;
+        document.getElementById('PLN').value = document.getElementById('BYN').value / plnId * 10;
+        document.getElementById('RUB').value = document.getElementById('BYN').value / rubId * 100;
+        document.getElementById('KZT').value = document.getElementById('BYN').value / kztId * 1000;
+    }
+};
