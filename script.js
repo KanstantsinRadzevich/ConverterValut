7d14077619e2b0be305e18b250e86711c6d2fcc9
Kanstantsin <Radzevich.k@gmail.com>
2021-05-27 09:58:37 +0300

    remove iframe google sheet, radio. Add UZS

--- script.js
+++ script.js
@@ -5,24 +5,35 @@ $(function() {
             w.document.write('Курсы обновлены на дату, указанную в календаре. Если ничего не выбрно - установлены курсы на сегодня!')
             w.focus()
             setTimeout(function() {w.close();}, 2000)
+            
+            rates2();    
+            
+            
+        });    
+        
+    });    
     
-        rates2();
-
-
-    });
-
-});
-
-function rates2(e) {
-
-    var curRates = [];
+    
+    function rates2(e) {
+        
+        var curRates = [];
+        /*rate(); if (item["Cur_ID"] == 345) */
+        var curIdUzs = 345;
+        $.getJSON(uri + 'ExRates/Rates/' + curIdUzs, { 'onDate':new Date($('#iDate').val()).toUTCString(), 'ParamMode': 0 })
+        .done(function (data) {
+            curRates.push(data.Cur_OfficialRate);
+         }).error(function (err) {
+               alert('ошибка');
+        });
+    
+        
     $.getJSON(uri + 'ExRates/Rates', {
             'onDate': new Date($('#iDate').val()).toUTCString(),
             'Periodicity': '0'
         })
         .done(function(data) {
             $.each(data, function(key, item) {
-                    console.log(data)
+                   
                 if (item["Cur_ID"] == 145) {
 
                     curRates.push(item["Cur_OfficialRate"])
@@ -43,31 +54,29 @@ function rates2(e) {
 
                     curRates.push(item["Cur_OfficialRate"])
                 }
-                if (item["Cur_ID"] == 345) {
-
-                    curRates.push(item["Cur_OfficialRate"])
-                }
-
+                
             });
 
-            console.log(curRates)
+
+            
             crossCur(curRates, e);
+            
         });
 
 
 }
 
 function crossCur(curRates, e) {
-
-
-    var usdId = parseFloat(curRates[0], 10); //USD
-    var eurId = parseFloat(curRates[1], 10); //EUR
-    var plnId = parseFloat(curRates[2], 10); //10pln
-    var rubId = parseFloat(curRates[3], 10); //100rub
-    var kztId = parseFloat(curRates[4], 10); //1000
-    var uzsId = parseFloat(curRates[5], 10); //10000
-
-
+   
+    var uzsId = parseFloat(curRates[0], 10); //10000
+    var usdId = parseFloat(curRates[1], 10); //USD
+    var eurId = parseFloat(curRates[2], 10); //EUR
+    var plnId = parseFloat(curRates[3], 10); //10pln
+    var rubId = parseFloat(curRates[4], 10); //100rub
+    var kztId = parseFloat(curRates[5], 10); //1000
+    
+    console.log(curRates)
+    
 
 
     if (e == "E") {
@@ -84,6 +93,7 @@ function crossCur(curRates, e) {
         document.getElementById('RUB').value = (document.getElementById('USD').value * usdId / eurId * eurId * 100 / rubId).toFixed(4);
         document.getElementById('PLN').value = (document.getElementById('USD').value * usdId / eurId * eurId * 10 / plnId).toFixed(4);
         document.getElementById('KZT').value = (document.getElementById('USD').value * usdId / eurId * eurId * 1000 / kztId).toFixed(4);
+        document.getElementById('UZS').value = (document.getElementById('USD').value * usrId / eurID * eurId * 10000 / uzsId).toFixed(4);
         document.getElementById('BYN').value = (document.getElementById('USD').value * usdId).toFixed(4);
     }
     if (e == "R") {
@@ -92,6 +102,7 @@ function crossCur(curRates, e) {
         document.getElementById('USD').value = (document.getElementById('RUB').value * rubId / 100 / usdId).toFixed(4);
         document.getElementById('PLN').value = (document.getElementById('RUB').value * rubId / 100 / plnId * 10).toFixed(4);
         document.getElementById('KZT').value = (document.getElementById('RUB').value * rubId / 100 / kztId * 1000).toFixed(4);
+        document.getElementById('UZS').value = (document.getElementById('RUB').value * rubId / 100 / uzsID * 10000).toFixed(4);
         document.getElementById('BYN').value = (document.getElementById('RUB').value * rubId / 100).toFixed(4);
 
     }
@@ -101,6 +112,7 @@ function crossCur(curRates, e) {
         document.getElementById('USD').value = (document.getElementById('PLN').value * plnId / 10 / usdId).toFixed(4);
         document.getElementById('RUB').value = (document.getElementById('PLN').value * plnId / 10 / rubId * 100).toFixed(4);
         document.getElementById('KZT').value = (document.getElementById('PLN').value * plnId / 10 / kztId * 1000).toFixed(4);
+        document.getElementById('UZS').value = (document.getElementById('PLN').value * plnId / 10 / uzsId * 1000).toFixed(4);
         document.getElementById('BYN').value = (document.getElementById('PLN').value * plnId / 10).toFixed(4);
     }
     if (e == "K") {
@@ -109,6 +121,7 @@ function crossCur(curRates, e) {
         document.getElementById('USD').value = (document.getElementById('KZT').value * kztId / 1000 / usdId).toFixed(4);
         document.getElementById('PLN').value = (document.getElementById('KZT').value * kztId / 1000 / plnId * 10).toFixed(4);
         document.getElementById('RUB').value = (document.getElementById('KZT').value * kztId / 1000 / rubId * 100).toFixed(4);
+        document.getElementById('UZS').value = (document.getElementById('KZT').value * kztId / 1000 / uzsId *10000).toFixed(4);
         document.getElementById('BYN').value = (document.getElementById('KZT').value * kztId / 1000).toFixed(4);
     }
     if (e == "UZ") {
@@ -127,6 +140,7 @@ function crossCur(curRates, e) {
         document.getElementById('PLN').value = (document.getElementById('BYN').value / plnId * 10).toFixed(4);
         document.getElementById('RUB').value = (document.getElementById('BYN').value / rubId * 100).toFixed(4);
         document.getElementById('KZT').value = (document.getElementById('BYN').value / kztId * 1000).toFixed(4);
+        document.getElementById('UZS').value = (document.getElementById('BYN').value / uzsId * 10000).toFixed(4);
     }
 };
 
@@ -139,13 +153,11 @@ function viewDiv(){
 
 //----Для получения одной валюты по коду. Буду использовать для узбекского сума  //
 
-  function rate(p) {
-    $.getJSON(uri + 'ExRates/Rates/' + $('#iCur').val(), { 'onDate': parseRuDate($('#iDate').val()).toUTCString(), 'ParamMode': p })
+  function rate() {
+    $.getJSON(uri + 'ExRates/Rates/' + $('#iCur').val(), { 'onDate': parseRuDate($('#iDate').val()).toUTCString(), 'ParamMode': 0 })
   .done(function (data) {
       $('<li>', { text: JSON.stringify(data) }).appendTo($('#res'));
-      $('#btn').removeAttr("disabled");
   }).error(function (err) {
-      $('#btn').removeAttr("disabled");
-      alert('ошибка');
+     alert('ошибка');
   });
 };
